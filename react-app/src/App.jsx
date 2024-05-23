f030ccd4cfb8fd09d62ac9b163534efdf3424cd4
AdityaKannur <adityakannur28@gmail.com>
2024-05-23 15:56:59 +0530

    desgning basic structure of navbar

---
+++ react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
