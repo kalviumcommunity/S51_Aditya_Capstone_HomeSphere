f471e2e834858d7ac2f30dbc70b37c88fac69f50
AdityaKannur <adityakannur28@gmail.com>
2024-06-21 10:29:25 +0530

    enabled third-party sign in

--- Frontend/react-app/src/Components/Navbar.jsx
+++ Frontend/react-app/src/Components/Navbar.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
 import './Navbar.css'
 
 
 function NavBar() {
 
+    const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+    if (isAuthenticated){
+        console.log(user)
+    }
+
+    const loggingout = () => {
+        logout({ logoutParams: { returnTo: window.location.origin }})
+        console.log("logged out")
+    }
+
     return (
         <>
             <div className='navBar'>
+
+                {/* <div>
+                {isAuthenticated ? <div>
+                    <img src={user.picture} alt='profile'></img> 
+                    <p>{user.name}</p>
+                </div> 
+                : "profile"}
+                </div> */}
+                
                 <div className='logo-container'>
                     <div>
                     <img src="" alt="Website Logo" />
@@ -23,10 +44,11 @@ function NavBar() {
 
                 <div className='additional-navitems-container'>
                     <div>
-                    <img src='' alt='sign up'></img>
-                    </div>
-                    <div>
-                        <img src='' alt='sign in'></img>
+                        {isAuthenticated?(
+                            <button onClick={loggingout }>Log out</button>
+                        ):(
+                            <button onClick={() => loginWithRedirect() }>Sign In</button>
+                        )}
                     </div>
                     <div>
                         <img src="" alt='profile'></img>
